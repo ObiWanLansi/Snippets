d0942b5d13b1dd8d80864658e912e9c8bcc0fbc6
Jörg Lanser <38795604+ObiWanLansi@users.noreply.github.com>
2019-02-08 07:15:32 +0100

    Update HelloMongoDb.py

--- Database/MongoDb/HelloMongoDb.py
+++ Database/MongoDb/HelloMongoDb.py
@@ -1,2 +1,15 @@
+import datetime
 from pymongo import MongoClient
-client = MongoClient()
+
+client = MongoClient('localhost', 27017)
+
+db = client['test']
+
+collection = db['test']
+
+post = {"author": "Lansi",
+        "text": "This Is The First Document!",
+        "tags": ["mongodb", "python", "pymongo", "linux", "pizza"],
+        "date": datetime.datetime.utcnow()}
+
+collection.insert_one(post)
