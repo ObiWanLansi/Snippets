9d8d53b430530be8bdbf81c08484591fdda38d53
Jörg Lanser <joerg.lanser@gmail.com>
2019-02-26 10:39:03 +0100

    Matlab/Octave + Powershell Example

---
+++ Matlab_Octave/HelloWorld.m
@@ -0,0 +1,4 @@
+[x,y] = meshgrid(-2:.2:2);
+g = x .* exp(-x.^2 - y.^2);
+surf(x, y, g)
+print -deps graph.eps
---
+++ Powershell/ClassExample.ps1
@@ -0,0 +1,16 @@
+﻿class Person
+{
+    [string] $Surename;
+
+    [string] $Firstname;
+
+    [DateTime] $DateOfBirth
+}
+
+
+$p = [Person]::new()
+$p.Firstname = "Hans"
+$p.Surename = "Glück"
+$p.DateOfBirth = [DateTime]::Now
+
+Out-GridView -InputObject $p
