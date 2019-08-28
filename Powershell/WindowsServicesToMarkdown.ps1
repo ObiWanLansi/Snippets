f1d933fda77029f6f6965352c87290582a291bb0
ObiWanLansi <joerg.lanser@gmail.com>
2019-08-28 23:00:29 +0200

    Create WindowsServicesToMarkdown.ps1

---
+++ Powershell/WindowsServicesToMarkdown.ps1
@@ -0,0 +1,23 @@
+﻿
+function Get-MarkdownForServices($status)
+{
+   $markdown = "`n# $status`n`n"
+   $markdown += "|Name|DisplayName|`n"
+   $markdown += "|----|-----------|`n"
+
+   foreach($service in Get-Service| Where-Object {$_.status -eq “$status”})
+   {
+        $markdown += "|" + $service.Name + "|" + $service.DisplayName + "|`n"
+   }
+
+    return $markdown
+}
+
+$markdown = ""
+
+$markdown += Get-MarkdownForServices("Running");
+$markdown += Get-MarkdownForServices("Stopped");
+
+
+$markdown
+Set-Clipboard $markdown
